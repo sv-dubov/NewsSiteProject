9ee686c78fa7b2e05c26cb339b405d9103d5c719
sv_dubov <sv_dubov@ukr.net>
2019-09-16 20:20:55 +0300

    Add roles

--- NewsSite/App_Start/Startup.cs
+++ NewsSite/App_Start/Startup.cs
@@ -19,6 +19,8 @@ namespace NewsSite
             // настраиваем контекст и менеджер
             app.CreatePerOwinContext<ApplicationContext>(ApplicationContext.Create);
             app.CreatePerOwinContext<ApplicationUserManager>(ApplicationUserManager.Create);
+            // регистрация менеджера ролей
+            app.CreatePerOwinContext<ApplicationRoleManager>(ApplicationRoleManager.Create);
             app.UseCookieAuthentication(new CookieAuthenticationOptions
             {
                 AuthenticationType = DefaultAuthenticationTypes.ApplicationCookie,
---
+++ NewsSite/Models/CreateRoleModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NewsSite.Models
+{
+    public class CreateRoleModel
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
